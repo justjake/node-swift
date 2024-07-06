0a2622e54768e27b937e2a12cc3261be68214ad8
Jake Teton-Landis <just.1.jake@gmail.com>
2024-07-06 14:31:16 -0400

    testing...

--- example/index.js
+++ example/index.js
@@ -1,4 +1,5 @@
-const { nums, str, add } = require("./.build/Module.node");
+const { nums, str, add, rect } = require("./.build/Module.node");
 console.log(nums); // [ 3, 4 ]
 console.log(str); // NodeSwift! NodeSwift! NodeSwift!
-add(5, 10).then(console.log); // 5.0 + 10.0 = 15.0
+console.log(rect, rect.origin, rect.size);
+// add(5, 10).then(console.log); // 5.0 + 10.0 = 15.0
